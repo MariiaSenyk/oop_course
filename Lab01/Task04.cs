19326e46a5e2055c7e76fe45db9e55887cc59cbc
Mariia Senyk <komamymi@gmail.com>
2026-09-10 20:30:10 +0300

    Lab01 Task04

---
+++ Lab01/Task04.cs
@@ -0,0 +1,28 @@
+namespace Lab01;
+
+public static class Task04
+{
+    public static void Run()
+    {
+        int systolic = int.Parse(Console.ReadLine()!);
+        int diastolic = int.Parse(Console.ReadLine()!);
+        string category;
+        if (systolic < 120 && diastolic < 80)
+        {
+            category = "норма";
+        }
+        else if (systolic < 130 && diastolic < 80)
+        {
+            category = "підвищений";
+        }
+        else if (systolic < 140 || diastolic < 90)
+        {
+            category = "гіпертонія 1 ступеня";
+        }
+        else 
+        {
+            category = "гіпертонія 2 ступеня";
+        }
+        Console.WriteLine($"Тиск: {systolic}/{diastolic} — {category}");
+    }
+}
