872d920126670c5a3f2d890418a3ad6bc0b9b694
Mariia Senyk <komamymi@gmail.com>
2026-09-14 19:25:02 +0300

    Lab02 Task02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -7,6 +7,6 @@ public static class Program
         System.Threading.Thread.CurrentThread.CurrentCulture =
             System.Globalization.CultureInfo.InvariantCulture;
 
-        Task01.Run();
+        Task02.Run();
     }
 }
