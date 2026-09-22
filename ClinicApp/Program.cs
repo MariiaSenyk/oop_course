908f4089a89bb5b466d9a3c96da99cd037c45f26
Mariia Senyk <komamymi@gmail.com>
2026-09-22 20:34:20 +0300

    Lab03 Task02

--- ClinicApp/Program.cs
+++ ClinicApp/Program.cs
@@ -18,5 +18,23 @@ public static class Program
         Console.WriteLine(p3);
         Console.WriteLine(p4);
         Console.WriteLine(p5);
+
+        Console.WriteLine();
+
+        Doctor d1 = new Doctor("Олег", "Сидоренко", "Кардіологія", "LIC-001", "0441234567");
+        d1.WorkEndHour = 16;
+
+        Doctor d2 = new Doctor("Наталія", "Мороз", "Неврологія", "LIC-002", "0442345678");
+        d2.WorkStartHour = 9;
+        d2.WorkEndHour = 18;
+
+        Doctor d3 = new Doctor("Андрій", "Власенко", "Педіатрія", "LIC-003", "0443456789");
+
+        Doctor d4 = new Doctor("Ірина", "Лисенко", "Терапія");
+
+        Console.WriteLine(d1);
+        Console.WriteLine(d2);
+        Console.WriteLine(d3);
+        Console.WriteLine(d4);
     }
 }
